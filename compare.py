42a5a7a60947f1a80f125c3646f4237d55525703
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-03-16 20:00:20 +0530

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,9 +1,9 @@
-True:
+while true:	
 	print("Enter 'x' for exit.")
 	string1 = input("Enter string1: ")
 	string2 = input("Enter string2: ")
 	if string1 == 'x':
-		break
+		
 	else:
 		if string1 == string2:
 			print("Both Strings are Equal.\n")

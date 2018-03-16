672644f6c011783492e2a3c1dd2444cbeb1f2b4f
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-03-16 19:28:05 +0530

    ten.py

---
+++ ten.py
@@ -0,0 +1,17 @@
+num1 = 20
+num2 = 30
+num3 = 40
+num4 = 50
+num5 = 60
+num6 = 70
+num7 = 80
+num8 = 90
+num9 = 10
+num10 = 12
+if (num1 >= num2) and (num2 >= num3):
+largest = num1
+elif:
+if (num2 >= num4) and (num3 >= num4):
+largest = num2
+else:
+Print("the largest number among the ten numbers")

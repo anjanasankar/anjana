43f02c4f2a4e3701f5ec56963ae2f87711d136e7
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-03-31 22:11:47 +0530

    palee

---
+++ palee.py
@@ -0,0 +1,14 @@
+def reverse(s):
+return s[::-1]
+def isPalindrome(s):
+rev = reverse;
+if (s == rev):
+return True
+return False
+s = "malayalam"
+ans = is palindrome(s)
+
+if ans == 1:
+    print("Yes")
+else:
+    print("N

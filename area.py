a72314d2e8f8285dc860d937b8f4b96764a229e7
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-03-31 22:17:26 +0530

    area

---
+++ area.py
@@ -0,0 +1,6 @@
+width = float(input('Please Enter the width of a farm: '))
+height = float(input('Please Enter the Height of a farm: '))
+Area = width * height
+Perimeter = 2 * (width + height)
+print("\n Area of a Rectangle is: %.2f" %Area)
+print(" Perimeter of Rectangle is: %.2f" %Perimeter)

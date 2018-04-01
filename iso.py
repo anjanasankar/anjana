946550f995b81c581eb9217948a442a261ea5b2e
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-04-01 15:21:50 +0530

    iso

---
+++ iso.py
@@ -0,0 +1,12 @@
+def is_isogram(word):
+if type(word) != str:
+raise TypeError('Argument should be a string')
+elif word == "":
+return (word, False)
+else:
+word = word.lower()
+for char in word:
+if word.count(char) > 1:
+return (word, False)
+else:
+return (word, True) 

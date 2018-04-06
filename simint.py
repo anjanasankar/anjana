baf9efea2eca02b1b200368310d45d1298ae78e6
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-04-06 16:07:37 +0530

    simint

---
+++ simint.py
@@ -0,0 +1,5 @@
+principle=float(input("Enter the principle amount:"))
+time=int(input("Enter the time(years):"))
+rate=float(input("Enter the rate:"))
+simple_interest=(principle*time*rate)/100
+print("the simple interest is:",simple_interest)
